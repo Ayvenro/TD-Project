a31cd96ad19151feeaf86e5334c90c5f3053bb29
FantomAda <fantomada@yandex.ru>
2022-02-14 15:54:08 +0300

    Check attacker on lane

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -1,9 +1,55 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
+    private AttackerSpawner myLaneSpawner;
+    private Animator animator;
+    private void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+        SetLaneSpawner();
+    }
+
+    private void Update()
+    {
+        if (IsAttackerInLane())
+        {
+            animator.SetBool("isAttacking", true);
+        }
+        else
+        {
+            animator.SetBool("isAttacking", false);
+        }
+    }
+
+    private void SetLaneSpawner()
+    {
+        AttackerSpawner[] spawners = FindObjectsOfType<AttackerSpawner>();
+        foreach (AttackerSpawner spawner in spawners)
+        {
+            bool isCloseEnough = Mathf.Abs(spawner.transform.position.y - transform.position.y) <= Mathf.Epsilon;
+            if (isCloseEnough)
+            {
+                myLaneSpawner = spawner;
+            }
+        }
+    }
+
+    private bool IsAttackerInLane()
+    {
+        if (myLaneSpawner.transform.childCount > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
     public void Fire(GameObject projectilePrefab)
     {
         Instantiate(projectilePrefab, transform.GetChild(0).position, transform.rotation);
