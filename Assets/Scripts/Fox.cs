c5db253df6716e400a4744842a0f6d4f8f4a1e79
FantomAda <fantomada@yandex.ru>
2022-02-17 16:32:44 +0300

    New attacker - Fox

---
+++ Assets/Scripts/Fox.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fox : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject otherObject = collision.gameObject;
+        if (otherObject.GetComponent<GraveStone>())
+        {
+            GetComponent<Animator>().SetTrigger("jumpTrigger");
+        }
+        else if (otherObject.GetComponent<Attacker>())
+        {
+            GetComponent<Attacker>().Attack(otherObject);
+        }
+    }
+}
